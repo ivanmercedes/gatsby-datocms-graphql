be37e2034950f47b065be63146ec37cec891a3e4
Ivan Mercedes <ivanmercede@gmail.com>
2020-10-11 02:51:57 +0200

    Adding style to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {css} from '@emotion/core';
 
 
 const Header = () => {
     return ( 
-        <h1>Desde Header</h1>
+        <header
+            css={css`
+                background-color: #333;
+                padding:1rem;
+            `}
+        >
+            <div
+                css={css`
+                max-width: 1200px;
+                margin: 0 auto;
+
+                @media (min-width: 768px){
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                }
+                `}
+            >
+                <h1
+                    css={css`
+                        color:#fff;
+                        text-align: center;git
+                    `}
+                >Hotel Gatsby</h1>
+
+            </div>
+        </header>
      );
 }
  
