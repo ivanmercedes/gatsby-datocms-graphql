91a27e42b358e8595cd354965a73992d3c880e41
Ivan Mercedes <ivanmercede@gmail.com>
2020-10-11 18:15:34 +0200

    Adding preview Hotel Component and get info from CMS

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { graphql, useStaticQuery }  from 'gatsby';
 import Image from 'gatsby-image';
 import styled from '@emotion/styled';
 import {css} from '@emotion/core'
@@ -22,24 +21,8 @@ const TextoInicio = styled.div`
 `;
 
 const ContenidoInicio = ({inicio}) => {
-    // const informacion = useStaticQuery(graphql`
-    // query{
-    //     allDatoCmsPagina(filter: {slug: {eq: "inicio" } } ){
-    //       nodes{
-    //         titulo
-    //         contenido
-    //         imagen{
-    //           fluid {
-    //             ...GatsbyDatoCmsFluid
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // `);
-    // // informacion.allDatoCmsPagina.nodes[0]
-
-     const { titulo, contenido, imagen } = inicio;
+   
+    const { titulo, contenido, imagen } = inicio;
     return ( 
         <>
             <h2
