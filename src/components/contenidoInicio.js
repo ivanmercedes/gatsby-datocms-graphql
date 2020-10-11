68aa2bc5230b18fb8f54175fb26143d87a619bae
Ivan Mercedes <ivanmercede@gmail.com>
2020-10-11 17:00:38 +0200

    Fix staticQuery Bug

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -21,25 +21,25 @@ const TextoInicio = styled.div`
     }
 `;
 
-const ContenidoInicio = () => {
-    const informacion = useStaticQuery(graphql`
-    query{
-        allDatoCmsPagina(filter: {slug: {eq: "inicio" } } ){
-          nodes{
-            titulo
-            contenido
-            imagen{
-              fluid {
-                ...GatsbyDatoCmsFluid
-              }
-            }
-          }
-        }
-      }
-    `);
-    // informacion.allDatoCmsPagina.nodes[0]
+const ContenidoInicio = ({inicio}) => {
+    // const informacion = useStaticQuery(graphql`
+    // query{
+    //     allDatoCmsPagina(filter: {slug: {eq: "inicio" } } ){
+    //       nodes{
+    //         titulo
+    //         contenido
+    //         imagen{
+    //           fluid {
+    //             ...GatsbyDatoCmsFluid
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // `);
+    // // informacion.allDatoCmsPagina.nodes[0]
 
-    const { titulo, contenido, imagen } = informacion.allDatoCmsPagina.nodes[0];
+     const { titulo, contenido, imagen } = inicio;
     return ( 
         <>
             <h2
